dbd6441a7b3980dfd48972db617e2f12210e1143
Gurjot Singh <gurjotsingh0806@gmail.com>
2019-07-26 22:37:55 -0400

    Revert "Design"

--- StudentHelper/MainActivity.cs
+++ StudentHelper/MainActivity.cs
@@ -3,71 +3,24 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
-using Android;
-using System;
 
 namespace StudentHelper
 {
-    [Activity(Label = "@string/app_name", MainLauncher = true)]
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : Activity
     {
-        [Obsolete]
-        Fragment[] _fragmentsArray;
-        string name = "Welcome To my App";
-
-
-
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            // Set our view from the "main" layout resource
-            RequestWindowFeature(Android.Views.WindowFeatures.ActionBar);
-            //enable navigation mode to support tab layout
-            this.ActionBar.NavigationMode = ActionBarNavigationMode.Tabs;
-
             base.OnCreate(savedInstanceState);
-
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
-
-
-            _fragmentsArray = new Fragment[]
-            {
-            new FirstFragment(""),
-            new SecondFragment(),
-            };
-
-
-            AddTabToActionBar("Sign In"); //First Tab
-            AddTabToActionBar("Sign Up"); //First Tab
-
-
         }
-
-
-        void AddTabToActionBar(string tabTitle)
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
-            Android.App.ActionBar.Tab tab = ActionBar.NewTab();
-            tab.SetText(tabTitle);
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
-            tab.SetIcon(Android.Resource.Drawable.IcInputAdd);
-
-            tab.TabSelected += TabOnTabSelected;
-
-            ActionBar.AddTab(tab);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
-
-
-
-        void TabOnTabSelected(object sender, Android.App.ActionBar.TabEventArgs tabEventArgs)
-        {
-            Android.App.ActionBar.Tab tab = (Android.App.ActionBar.Tab)sender;
-
-            //Log.Debug(Tag, "The tab {0} has been selected.", tab.Text);
-            Fragment frag = _fragmentsArray[tab.Position];
-
-            tabEventArgs.FragmentTransaction.Replace(Resource.Id.frameLayout1, frag);
-        }
-
-
-
     }
 }
